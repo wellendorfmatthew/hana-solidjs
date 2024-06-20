13413ef616af873ae543c532978f251bc291ebd8
nixowl <gabriellamondvay@gmail.com>
2024-06-20 21:28:05 +0200

    add homepage and carousel component with carousel content

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,3 +1,30 @@
+import { Carousel } from "../components/Carousel";
+import carousel1 from "../assets/images/carousel1.jpg";
+import carousel2 from "../assets/images/carousel2.jpeg";
+import carousel3 from "../assets/images/carousel3.jpeg";
+
+const homeSlides = [
+  {
+    alt: "Rainbow wall",
+    text: "Connect with other members of the LGBTQIA+ community in a safe space.",
+    image: carousel1,
+  },
+  {
+    alt: "puzzle pieces",
+    text: "Get personalized resources that will help you discover more about yourself.",
+    image: carousel2,
+  },
+  {
+    alt: "Hand holding heart",
+    text: "Find information on mental health support.",
+    image: carousel3,
+  },
+];
+
 export default function Homepage() {
-    
+    return (
+        <div class="section-container">
+            <Carousel slides={homeSlides} />
+        </div>
+    )
  }
