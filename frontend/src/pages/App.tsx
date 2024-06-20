808cfc1173d52011212a8eee509605670b7d5b14
nixowl <gabriellamondvay@gmail.com>
2024-06-20 15:04:17 +0200

    add embla carousel

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,20 +1,19 @@
-import { useNavigate } from '@solidjs/router';
-
+import { useNavigate } from "@solidjs/router";
 
 export default function App() {
   const navigate = useNavigate();
 
   return (
-    <div class='section-container gap-60'>
-      <p class='hana-title'>Hana</p>
-      <div class='flex flex-col gap-6 w-[200px]'>
-        <button class='btn-primary' onClick={() => navigate("/phonenumber")}>
+    <div class="section-container gap-60">
+      <p class="hana-title">Hana</p>
+      <div class="flex flex-col gap-6 w-[200px]">
+        <button class="btn-primary" onClick={() => navigate("/phonenumber")}>
           Sign In
         </button>
-        <button class='btn-primary' onClick={() => navigate("/phonenumber")}>
+        <button class="btn-primary" onClick={() => navigate("/phonenumber")}>
           Sign Up
         </button>
       </div>
     </div>
-  )
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'hana-bg': '#14042D',
-        'btn-primary': '#8C5AD9',
-        'btn-secondary': '#D9A059',
-        'input-bg': '#E4D2FA',
-        'btn-disabled': '#9EB1A4',
-        'selection-bg': '#59D977'
+        "hana-bg": "#14042D",
+        "btn-bg-primary": "#8C5AD9",
+        "btn-bg-secondary": "#D9A059",
+        "input-bg": "#E4D2FA",
+        "btn-bg-disabled": "#9EB1A4",
+        "selection-bg": "#59D977",
       },
       fontFamily: {
-        'hana-logo': ['Lemon', 'serif'],
-        'hana-text': ['Grotesque', 'sans-serif']
-      }
+        "hana-logo": ["Lemon", "serif"],
+        "hana-text": ["Grotesque", "sans-serif"],
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
