e101f266e4efc0306ca08390dfca077949623527
nixowl <gabriellamondvay@gmail.com>
2024-06-20 21:29:04 +0200

    add arrow icons

---
+++ frontend/src/components/ui/Icons.tsx
@@ -0,0 +1,39 @@
+export function ArrowRight() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-move-right stroke-btn-bg-secondary"
+      >
+        <path d="M18 8L22 12L18 16" />
+        <path d="M2 12H22" />
+      </svg>
+    );
+}
+
+export function ArrowLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-move-left stroke-btn-bg-secondary"
+    >
+      <path d="M6 8L2 12L6 16" />
+      <path d="M2 12H22" />
+    </svg>
+  );
+}
