f4cc84a3a9ab1e728a33888bd1cc443fdaa6047a
nixowl <gabriellamondvay@gmail.com>
2024-06-20 21:29:34 +0200

    add carousel component with functional arrows and dots to show index

---
+++ frontend/src/components/Carousel.tsx
@@ -0,0 +1,81 @@
+import createEmblaCarousel from "embla-carousel-solid";
+import { createSignal, onMount } from "solid-js";
+import { ArrowLeft, ArrowRight } from "./ui/Icons";
+interface slide {
+  alt: string;
+  text: string;
+  image: string;
+}
+
+interface slides {
+  slides: slide[];
+}
+
+export function Carousel({ slides }: slides) {
+  const [emblaRef, emblaApi] = createEmblaCarousel();
+  const [activeIndex, setActiveIndex] = createSignal(0);
+
+
+  onMount(() => {
+    const api = emblaApi();
+    if (api) {
+      api.on("select", () => {
+        const number = api.selectedScrollSnap;
+        setActiveIndex(number);
+      });
+    }
+  });
+
+  const scrollPrev = () => {
+    const api = emblaApi();
+    if (api) {
+      api.scrollPrev();
+      console.log(api.slidesInView());
+    }
+  };
+
+  const scrollNext = () => {
+    const api = emblaApi();
+    if (api) {
+      api.scrollNext();
+      console.log(api.slidesInView());
+    }
+  };
+
+  return (
+    <div class="embla h-[97%] w-[90%]" ref={emblaRef}>
+      <div class="embla__container">
+        {slides.map((slide) => (
+          <div class="embla__slide flex flex-col gap-6">
+            <p class="font-hana-text text-white text-4xl text-balance">
+              {slide.text}
+            </p>
+            <img
+              src={slide.image}
+              alt={slide.alt}
+              class="rounded-lg mx-auto w-[55%] h-[580px] object-cover md:h-[530]"
+            />
+          </div>
+        ))}
+      </div>
+      <div class="flex justify-around text-white">
+        <button class="embla__prev" onClick={scrollPrev}>
+          <ArrowLeft />
+        </button>
+        <div>
+          {slides.map((_, index) => (
+            <button
+              class={`embla__dot ${
+                activeIndex() === index ? "active" : ""
+              }`}
+              onClick={() => emblaApi()?.scrollTo(index)}
+            ></button>
+          ))}
+        </div>
+        <button class="embla__next" onClick={scrollNext}>
+          <ArrowRight />
+        </button>
+      </div>
+    </div>
+  );
+}
