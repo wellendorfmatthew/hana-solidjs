aa237b92a5f9f9955f8cae5e94351cc38bd63341
nixowl <gabriellamondvay@gmail.com>
2024-06-28 12:58:11 +0200

    remove textarea type from input

--- frontend/src/components/ui/TextInput.tsx
+++ frontend/src/components/ui/TextInput.tsx
@@ -1,33 +1,50 @@
 import { JSX } from "solid-js";
+import { cn } from "../../lib/utils";
 
 interface textInputProps {
-  placeholder?: string;
+  placeholderText?: string;
   value?: string;
-  onChange?: (e: Event) => void;
+  onChange?: (e: Event & { currentTarget: HTMLInputElement }) => void;
   type?: string;
+  className?: string;
+  disabled?: boolean;
+  required?: boolean;
+  autofocus?: boolean;
 }
 
 /**
- * Text input component that can be used for any input type. It is prestyled but can be styled further with the regular 'class="style"' attribute
+ * **Text input component that can be used for any input type. It is prestyled but can be styled further with the regular** `class="style"` **attribute**
  * @param placeholder The placeholder text
- * @param value The current value of the input field, typically a state variable like [value, setValue] = createSignal("")
- * @param onChange The function to call when the input field changes, typically a function that sets the value of a state variable such as (e) => setValue(e.target.value)
- * @param type The type of input field, defaults to text but can be changed to password, email, textarea etc.
+ * @param value The current value of the input field, typically a state variable like `[value, setValue] = createSignal("")`
+ * @param onChange The function to call when the input field changes, typically a function that sets the value of a state variable such as `(e) => setValue(e.target.value)`
+ * @param autofocus The autofocus attribute, only one element per page can have this attribute. The element with this attribute will be focused on page load.
+ * @param type The type of input field, defaults to text but can be changed to password, email, etc.
+ * @param className This prop can be ignored, it is used to merge the default styles with any additional styles passed in. Use the normal `class` attribute to add styles.
  * @returns An input field of type JSX.Element
  */
 export function TextInput({
-  placeholder,
-  value,
+  placeholderText,
+  value = "",
   onChange,
   type = "text",
+  className,
+  disabled,
+  required,
+  autofocus,
 }: textInputProps): JSX.Element {
   return (
     <input
+      autofocus={autofocus}
+      disabled={disabled}
+      required={required}
       type={type}
-      class="input"
+      class={cn(
+        "h-10 w-[30%] rounded-md bg-input-bg px-3 py-2 text-sm ring-offset-background  placeholder:text-black/80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
       value={value}
       onChange={onChange}
-      placeholder={placeholder}
+      placeholder={placeholderText}
     />
   );
 }
