bbfd2ff2aa4cc6dfc50e05be5fe72d02ffada2c7
MarMel <melikidouria@gmail.com>
2024-07-03 13:36:41 +0300

    added padding to the radio button component

--- frontend/src/components/ui/RadioButton.tsx
+++ frontend/src/components/ui/RadioButton.tsx
@@ -14,7 +14,7 @@ export function RadioButton({userOption}: optionProps) {
             {userOption.map((option) => {
                 return (
                     <div class="relative list-none select-none p-2 mb-3 text-center option focus:bg-green-neon/100 focus:text-purple-dark focus:font-semibold" tabindex={0}>
-                        <label for={option.id} class="font-normal text-2xl">{option.value}</label>
+                        <label for={option.id} class="font-normal p-2 text-2xl">{option.value}</label>
                         <input type="radio" id={option.id} name={option.name} value={option.value} class="hidden" />
                     </div>
                 )
