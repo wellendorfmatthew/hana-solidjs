e365546f16e016143ea6f0486fcf4260953afbd3
MarMel <melikidouria@gmail.com>
2024-07-04 06:29:20 +0300

    changes in Dropdown.tsx in an attempt to close menu when user clicks outside it

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,4 +1,8 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
+
+let menuOuterRef: HTMLDivElement;
+let menuRef: HTMLDivElement;
+let windowRef: Document;
 
 interface optionInfo {
     name: string;
@@ -10,17 +14,22 @@ interface menuProps {
     menuOption: optionInfo[];
     placeholder?: string;
 }
-
-export function Dropdown({menuOption}: menuProps) {
+    
+export function Dropdown({ menuOption }: menuProps) {
+    
     const [menuIsOpen, setMenuIsOpen] = createSignal(true);
     const [selected, setSelected] = createSignal("")
 
+    createEffect(() => {
+        windowRef.onclick ? (() => { menuRef.hidden = setMenuIsOpen(true) }) : null;
+    })
+
     return (
         <>
-            <div class="relative trigger menu bg-[url('assets/images/caretDown.svg')] bg-no-repeat bg-[left_11rem_top_57%] bg-[length:1rem_1rem] hover:cursor-pointer" onClick={(prev) => setMenuIsOpen(!prev)}>
+            <div ref={menuOuterRef} class="relative trigger menu bg-[url('assets/images/caretDown.svg')] bg-no-repeat bg-[left_11rem_top_57%] bg-[length:1rem_1rem] hover:cursor-pointer" onClick={(prev) => setMenuIsOpen(!prev)}> 
                 <span class="px-3 font-hana-text text-green-neon font-medium text-2xl">{selected() ? selected() : "Choose an option"}</span>
             </div>
-            <div class="absolute menu top-16 mt-1 z-10 size-34 w-[13rem] p-0" classList={{ "hidden": menuIsOpen() }}>
+            <div ref={menuRef} class="absolute menu top-16 mt-1 z-10 size-34 w-[13rem] p-0" classList={{ "hidden": menuIsOpen() }}>
                 <ul class="overflow-y-auto menucontent h-32">
                     {menuOption.map((option) => {
                         return (
